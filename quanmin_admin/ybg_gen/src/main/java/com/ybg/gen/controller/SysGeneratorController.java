ee8ebedb4baea3998a1c4dfa4ca570629f01eba1
Deament <591518884@qq.com>
2017-06-30 07:38:22 +0800

    no commit message

--- quanmin_admin/ybg_gen/src/main/java/com/ybg/gen/controller/SysGeneratorController.java
+++ quanmin_admin/ybg_gen/src/main/java/com/ybg/gen/controller/SysGeneratorController.java
@@ -1,11 +1,16 @@
 package com.ybg.gen.controller;
 import com.alibaba.fastjson.JSON;
+import com.ybg.base.util.Page;
+import com.ybg.gen.qvo.GeneratorQuery;
 import com.ybg.gen.service.SysGeneratorService;
 import com.ybg.gen.utils.xss.XssHttpServletRequestWrapper;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
@@ -20,28 +25,22 @@ import java.util.Map;
  * @email sunlightcs@gmail.com
  * @date 2016年12月19日 下午9:12:58 */
 @Controller
-@RequestMapping("/sys/generator")
+@RequestMapping("/sys/generator_do/")
 public class SysGeneratorController {
 	
 	@Autowired
 	private SysGeneratorService sysGeneratorService;
 	
-	// /**
-	// * 列表
-	// */
-	// @ResponseBody
-	// @RequestMapping("/list")
-	//
-	// public R list(@RequestParam Map<String, Object> params){
-	// //查询列表数据
-	// Query query = new Query(params);
-	// List<Map<String, Object>> list = sysGeneratorService.queryList(query);
-	// int total = sysGeneratorService.queryTotal(query);
-	//
-	// PageUtils pageUtil = new PageUtils(list, total, query.getLimit(), query.getPage());
-	//
-	// return R.ok().put("page", pageUtil);
-	// }
+	@ResponseBody
+	@RequestMapping(value = "list.do", method = { RequestMethod.GET, RequestMethod.POST })
+	public Page list(@ModelAttribute GeneratorQuery qvo, @RequestParam(name = "pageNow", required = false, defaultValue = "0") Integer pageNow, ModelMap map) {
+		Page page = new Page();
+		page.setCurPage(pageNow);
+		page = sysGeneratorService.list(page, qvo);
+		page.init();
+		return page;
+	}
+	
 	/** 生成代码 */
 	@RequestMapping("/code")
 	public void code(HttpServletRequest request, HttpServletResponse response) throws IOException {
