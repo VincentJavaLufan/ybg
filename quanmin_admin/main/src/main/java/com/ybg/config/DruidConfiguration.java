c7796d1213de2bd814e74d609326e690ec15d2a8
Deament <591518884@qq.com>
2017-06-30 11:57:34 +0800

    更新配置

--- quanmin_admin/main/src/main/java/com/ybg/config/DruidConfiguration.java
+++ quanmin_admin/main/src/main/java/com/ybg/config/DruidConfiguration.java
@@ -1,28 +1,145 @@
 package com.ybg.config;
+
+import com.alibaba.druid.filter.Filter;
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import com.alibaba.druid.wall.WallConfig;
+import com.alibaba.druid.wall.WallFilter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.alibaba.druid.support.http.StatViewServlet;
-import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
-/** druid 配置.
- * 
+import javax.sql.DataSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Druid配置
  *
- * 
- * 这样的方式不需要添加注解：@ServletComponentScan
- * 
- * @author Administrator */
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017-04-21 0:00
+ */
 @Configuration
 public class DruidConfiguration {
-	
-	/** 注册一个StatViewServlet
-	 * 
-	 * @return */
-	@Bean
-	public ServletRegistrationBean DruidStatViewServle2() {
-		// org.springframework.boot.context.embedded.ServletRegistrationBean提供类的进行注册.
-		ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+
+    private Logger logger = LoggerFactory.getLogger(DruidConfiguration.class);
+
+    @Value("${spring.datasource.url:#{null}}")
+    private String dbUrl;
+    @Value("${spring.datasource.username: #{null}}")
+    private String username;
+    @Value("${spring.datasource.password:#{null}}")
+    private String password;
+    @Value("${spring.datasource.driverClassName:#{null}}")
+    private String driverClassName;
+    @Value("${spring.datasource.initialSize:#{null}}")
+    private Integer initialSize;
+    @Value("${spring.datasource.minIdle:#{null}}")
+    private Integer minIdle;
+    @Value("${spring.datasource.maxActive:#{null}}")
+    private Integer maxActive;
+    @Value("${spring.datasource.maxWait:#{null}}")
+    private Integer maxWait;
+    @Value("${spring.datasource.timeBetweenEvictionRunsMillis:#{null}}")
+    private Integer timeBetweenEvictionRunsMillis;
+    @Value("${spring.datasource.minEvictableIdleTimeMillis:#{null}}")
+    private Integer minEvictableIdleTimeMillis;
+    @Value("${spring.datasource.validationQuery:#{null}}")
+    private String validationQuery;
+    @Value("${spring.datasource.testWhileIdle:#{null}}")
+    private Boolean testWhileIdle;
+    @Value("${spring.datasource.testOnBorrow:#{null}}")
+    private Boolean testOnBorrow;
+    @Value("${spring.datasource.testOnReturn:#{null}}")
+    private Boolean testOnReturn;
+    @Value("${spring.datasource.poolPreparedStatements:#{null}}")
+    private Boolean poolPreparedStatements;
+    @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize:#{null}}")
+    private Integer maxPoolPreparedStatementPerConnectionSize;
+    @Value("${spring.datasource.filters:#{null}}")
+    private String filters;
+    @Value("{spring.datasource.connectionProperties:#{null}}")
+    private String connectionProperties;
+
+    @Bean     //声明其为Bean实例
+    @Primary  //在同样的DataSource中，首先使用被标注的DataSource
+
+    public DataSource dataSource(){
+        DruidDataSource datasource = new DruidDataSource();
+
+        datasource.setUrl(this.dbUrl);
+        datasource.setUsername(username);
+        datasource.setPassword(password);
+        datasource.setDriverClassName(driverClassName);
+        //configuration
+        if(initialSize != null) {
+            datasource.setInitialSize(initialSize);
+        }
+        if(minIdle != null) {
+            datasource.setMinIdle(minIdle);
+        }
+        if(maxActive != null) {
+            datasource.setMaxActive(maxActive);
+        }
+        if(maxWait != null) {
+            datasource.setMaxWait(maxWait);
+        }
+        if(timeBetweenEvictionRunsMillis != null) {
+            datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        }
+        if(minEvictableIdleTimeMillis != null) {
+            datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        }
+        if(validationQuery!=null) {
+            datasource.setValidationQuery(validationQuery);
+        }
+        if(testWhileIdle != null) {
+            datasource.setTestWhileIdle(testWhileIdle);
+        }
+        if(testOnBorrow != null) {
+            datasource.setTestOnBorrow(testOnBorrow);
+        }
+        if(testOnReturn != null) {
+            datasource.setTestOnReturn(testOnReturn);
+        }
+        if(poolPreparedStatements != null) {
+            datasource.setPoolPreparedStatements(poolPreparedStatements);
+        }
+        if(maxPoolPreparedStatementPerConnectionSize != null) {
+            datasource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+        }
+
+        if(connectionProperties != null) {
+            datasource.setConnectionProperties(connectionProperties);
+        }
+
+        List<Filter> filters = new ArrayList<>();
+        filters.add(statFilter());
+        filters.add(wallFilter());
+        datasource.setProxyFilters(filters);
+
+        return datasource;
+    }
+
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+//        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+//        servletRegistrationBean.setServlet(new StatViewServlet());
+//        servletRegistrationBean.addUrlMappings("/druid/*");
+    	
+    	//下面是本人的
+    	ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
 		// 添加初始化参数：initParams
 		// 白名单：
 		//servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
@@ -34,9 +151,37 @@ public class DruidConfiguration {
 		// 是否能够重置数据.
 		servletRegistrationBean.addInitParameter("resetEnable", "false");
 		return servletRegistrationBean;
-	}
-	
-	/** 注册一个：filterRegistrationBean
+      
+    }
+
+    @Bean
+    public StatFilter statFilter(){
+        StatFilter statFilter = new StatFilter();
+        statFilter.setLogSlowSql(true);
+        statFilter.setMergeSql(true);
+        statFilter.setSlowSqlMillis(1000);
+
+        return statFilter;
+    }
+
+    @Bean
+    public WallFilter wallFilter(){
+        WallFilter wallFilter = new WallFilter();
+
+        //允许执行多条SQL
+        WallConfig config = new WallConfig();
+        config.setMultiStatementAllow(true);
+        wallFilter.setConfig(config);
+
+        return wallFilter;
+    }
+
+    @Bean
+    @Primary
+    public DataSourceTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+    /** 注册一个：filterRegistrationBean
 	 * 
 	 * @return */
 	@Bean
