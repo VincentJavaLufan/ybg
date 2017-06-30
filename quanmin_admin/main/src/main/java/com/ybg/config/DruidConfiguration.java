9f239ebe66efd865c8510a4cc11425dc4bb0b422
Deament <591518884@qq.com>
2017-06-30 12:01:13 +0800

    no commit message

--- quanmin_admin/main/src/main/java/com/ybg/config/DruidConfiguration.java
+++ quanmin_admin/main/src/main/java/com/ybg/config/DruidConfiguration.java
@@ -33,117 +33,124 @@ import java.util.List;
 @Configuration
 public class DruidConfiguration {
 
-    private Logger logger = LoggerFactory.getLogger(DruidConfiguration.class);
+	private Logger logger = LoggerFactory.getLogger(DruidConfiguration.class);
 
-    @Value("${spring.datasource.url:#{null}}")
-    private String dbUrl;
-    @Value("${spring.datasource.username: #{null}}")
-    private String username;
-    @Value("${spring.datasource.password:#{null}}")
-    private String password;
-    @Value("${spring.datasource.driverClassName:#{null}}")
-    private String driverClassName;
-    @Value("${spring.datasource.initialSize:#{null}}")
-    private Integer initialSize;
-    @Value("${spring.datasource.minIdle:#{null}}")
-    private Integer minIdle;
-    @Value("${spring.datasource.maxActive:#{null}}")
-    private Integer maxActive;
-    @Value("${spring.datasource.maxWait:#{null}}")
-    private Integer maxWait;
-    @Value("${spring.datasource.timeBetweenEvictionRunsMillis:#{null}}")
-    private Integer timeBetweenEvictionRunsMillis;
-    @Value("${spring.datasource.minEvictableIdleTimeMillis:#{null}}")
-    private Integer minEvictableIdleTimeMillis;
-    @Value("${spring.datasource.validationQuery:#{null}}")
-    private String validationQuery;
-    @Value("${spring.datasource.testWhileIdle:#{null}}")
-    private Boolean testWhileIdle;
-    @Value("${spring.datasource.testOnBorrow:#{null}}")
-    private Boolean testOnBorrow;
-    @Value("${spring.datasource.testOnReturn:#{null}}")
-    private Boolean testOnReturn;
-    @Value("${spring.datasource.poolPreparedStatements:#{null}}")
-    private Boolean poolPreparedStatements;
-    @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize:#{null}}")
-    private Integer maxPoolPreparedStatementPerConnectionSize;
-    @Value("${spring.datasource.filters:#{null}}")
-    private String filters;
-    @Value("{spring.datasource.connectionProperties:#{null}}")
-    private String connectionProperties;
+	@Value("${spring.datasource.url:#{null}}")
+	private String dbUrl;
+	@Value("${spring.datasource.username: #{null}}")
+	private String username;
+	@Value("${spring.datasource.password:#{null}}")
+	private String password;
+	@Value("${spring.datasource.driverClassName:#{null}}")
+	private String driverClassName;
+	@Value("${spring.datasource.initialSize:#{null}}")
+	private Integer initialSize;
+	@Value("${spring.datasource.minIdle:#{null}}")
+	private Integer minIdle;
+	@Value("${spring.datasource.maxActive:#{null}}")
+	private Integer maxActive;
+	@Value("${spring.datasource.maxWait:#{null}}")
+	private Integer maxWait;
+	@Value("${spring.datasource.timeBetweenEvictionRunsMillis:#{null}}")
+	private Integer timeBetweenEvictionRunsMillis;
+	@Value("${spring.datasource.minEvictableIdleTimeMillis:#{null}}")
+	private Integer minEvictableIdleTimeMillis;
+	@Value("${spring.datasource.validationQuery:#{null}}")
+	private String validationQuery;
+	@Value("${spring.datasource.testWhileIdle:#{null}}")
+	private Boolean testWhileIdle;
+	@Value("${spring.datasource.testOnBorrow:#{null}}")
+	private Boolean testOnBorrow;
+	@Value("${spring.datasource.testOnReturn:#{null}}")
+	private Boolean testOnReturn;
+	@Value("${spring.datasource.poolPreparedStatements:#{null}}")
+	private Boolean poolPreparedStatements;
+	@Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize:#{null}}")
+	private Integer maxPoolPreparedStatementPerConnectionSize;
+	@Value("${spring.datasource.filters:#{null}}")
+	private String filters;
+	@Value("{spring.datasource.connectionProperties:#{null}}")
+	private String connectionProperties;
 
-    @Bean     //声明其为Bean实例
-    @Primary  //在同样的DataSource中，首先使用被标注的DataSource
+	@Bean
+	// 声明其为Bean实例
+	@Primary
+	// 在同样的DataSource中，首先使用被标注的DataSource
+	public DataSource dataSource() {
+		DruidDataSource datasource = new DruidDataSource();
 
-    public DataSource dataSource(){
-        DruidDataSource datasource = new DruidDataSource();
+		datasource.setUrl(this.dbUrl);
+		datasource.setUsername(username);
+		datasource.setPassword(password);
+		datasource.setDriverClassName(driverClassName);
+		// configuration
+		if (initialSize != null) {
+			datasource.setInitialSize(initialSize);
+		}
+		if (minIdle != null) {
+			datasource.setMinIdle(minIdle);
+		}
+		if (maxActive != null) {
+			datasource.setMaxActive(maxActive);
+		}
+		if (maxWait != null) {
+			datasource.setMaxWait(maxWait);
+		}
+		if (timeBetweenEvictionRunsMillis != null) {
+			datasource
+					.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+		}
+		if (minEvictableIdleTimeMillis != null) {
+			datasource
+					.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+		}
+		if (validationQuery != null) {
+			datasource.setValidationQuery(validationQuery);
+		}
+		if (testWhileIdle != null) {
+			datasource.setTestWhileIdle(testWhileIdle);
+		}
+		if (testOnBorrow != null) {
+			datasource.setTestOnBorrow(testOnBorrow);
+		}
+		if (testOnReturn != null) {
+			datasource.setTestOnReturn(testOnReturn);
+		}
+		if (poolPreparedStatements != null) {
+			datasource.setPoolPreparedStatements(poolPreparedStatements);
+		}
+		if (maxPoolPreparedStatementPerConnectionSize != null) {
+			datasource
+					.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+		}
 
-        datasource.setUrl(this.dbUrl);
-        datasource.setUsername(username);
-        datasource.setPassword(password);
-        datasource.setDriverClassName(driverClassName);
-        //configuration
-        if(initialSize != null) {
-            datasource.setInitialSize(initialSize);
-        }
-        if(minIdle != null) {
-            datasource.setMinIdle(minIdle);
-        }
-        if(maxActive != null) {
-            datasource.setMaxActive(maxActive);
-        }
-        if(maxWait != null) {
-            datasource.setMaxWait(maxWait);
-        }
-        if(timeBetweenEvictionRunsMillis != null) {
-            datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
-        }
-        if(minEvictableIdleTimeMillis != null) {
-            datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
-        }
-        if(validationQuery!=null) {
-            datasource.setValidationQuery(validationQuery);
-        }
-        if(testWhileIdle != null) {
-            datasource.setTestWhileIdle(testWhileIdle);
-        }
-        if(testOnBorrow != null) {
-            datasource.setTestOnBorrow(testOnBorrow);
-        }
-        if(testOnReturn != null) {
-            datasource.setTestOnReturn(testOnReturn);
-        }
-        if(poolPreparedStatements != null) {
-            datasource.setPoolPreparedStatements(poolPreparedStatements);
-        }
-        if(maxPoolPreparedStatementPerConnectionSize != null) {
-            datasource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
-        }
+		if (connectionProperties != null) {
+			datasource.setConnectionProperties(connectionProperties);
+		}
 
-        if(connectionProperties != null) {
-            datasource.setConnectionProperties(connectionProperties);
-        }
+		List<Filter> filters = new ArrayList<>();
+		filters.add(statFilter());
+		filters.add(wallFilter());
+		datasource.setProxyFilters(filters);
 
-        List<Filter> filters = new ArrayList<>();
-        filters.add(statFilter());
-        filters.add(wallFilter());
-        datasource.setProxyFilters(filters);
+		return datasource;
+	}
 
-        return datasource;
-    }
+	@Bean
+	public ServletRegistrationBean druidServlet() {
+		// ServletRegistrationBean servletRegistrationBean = new
+		// ServletRegistrationBean();
+		// servletRegistrationBean.setServlet(new StatViewServlet());
+		// servletRegistrationBean.addUrlMappings("/druid/*");
 
-    @Bean
-    public ServletRegistrationBean druidServlet() {
-//        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
-//        servletRegistrationBean.setServlet(new StatViewServlet());
-//        servletRegistrationBean.addUrlMappings("/druid/*");
-    	
-    	//下面是本人的
-    	ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+		// 下面是本人的
+		ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(
+				new StatViewServlet(), "/druid/*");
 		// 添加初始化参数：initParams
 		// 白名单：
-		//servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
-		// IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
+		// servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
+		// IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not
+		// permitted to view this page.
 		servletRegistrationBean.addInitParameter("deny", "192.168.1.73");
 		// 登录查看信息的账号密码.
 		servletRegistrationBean.addInitParameter("loginUsername", "admin2");
@@ -151,46 +158,51 @@ public class DruidConfiguration {
 		// 是否能够重置数据.
 		servletRegistrationBean.addInitParameter("resetEnable", "false");
 		return servletRegistrationBean;
-      
-    }
 
-    @Bean
-    public StatFilter statFilter(){
-        StatFilter statFilter = new StatFilter();
-        statFilter.setLogSlowSql(true);
-        statFilter.setMergeSql(true);
-        statFilter.setSlowSqlMillis(1000);
+	}
 
-        return statFilter;
-    }
+	@Bean
+	public StatFilter statFilter() {
+		StatFilter statFilter = new StatFilter();
+		statFilter.setLogSlowSql(true);
+		statFilter.setMergeSql(true);
+		statFilter.setSlowSqlMillis(1000);
 
-    @Bean
-    public WallFilter wallFilter(){
-        WallFilter wallFilter = new WallFilter();
+		return statFilter;
+	}
 
-        //允许执行多条SQL
-        WallConfig config = new WallConfig();
-        config.setMultiStatementAllow(true);
-        wallFilter.setConfig(config);
+	@Bean
+	public WallFilter wallFilter() {
+		WallFilter wallFilter = new WallFilter();
 
-        return wallFilter;
-    }
+		// 允许执行多条SQL
+		WallConfig config = new WallConfig();
+		config.setMultiStatementAllow(true);
+		wallFilter.setConfig(config);
 
-    @Bean
-    @Primary
-    public DataSourceTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource());
-    }
-    /** 注册一个：filterRegistrationBean
+		return wallFilter;
+	}
+
+	@Bean
+	@Primary
+	public DataSourceTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(dataSource());
+	}
+
+	/**
+	 * 注册一个：filterRegistrationBean
 	 * 
-	 * @return */
+	 * @return
+	 */
 	@Bean
 	public FilterRegistrationBean druidStatFilter2() {
-		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(
+				new WebStatFilter());
 		// 添加过滤规则.
 		filterRegistrationBean.addUrlPatterns("/*");
 		// 添加不需要忽略的格式信息.
-		filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid2/*");
+		filterRegistrationBean.addInitParameter("exclusions",
+				"*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid2/*");
 		return filterRegistrationBean;
 	}
 }
