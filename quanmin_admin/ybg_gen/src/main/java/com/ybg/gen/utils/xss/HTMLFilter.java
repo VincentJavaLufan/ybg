485b2381a316607d072cc46cc826ce4ce2250311
Deament <591518884@qq.com>
2017-07-08 19:33:31 +0800

    no commit message

--- quanmin_admin/ybg_gen/src/main/java/com/ybg/gen/utils/xss/HTMLFilter.java
+++ quanmin_admin/ybg_gen/src/main/java/com/ybg/gen/utils/xss/HTMLFilter.java
@@ -1,5 +1,10 @@
 package com.ybg.gen.utils.xss;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.logging.Logger;
