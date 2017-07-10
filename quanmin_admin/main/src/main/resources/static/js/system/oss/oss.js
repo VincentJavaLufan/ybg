701c12075dfa15cd1f12749151cabf0e1405dc74
Deament <591518884@qq.com>
2017-07-10 23:45:07 +0800

    oss保存图片

--- quanmin_admin/main/src/main/resources/static/js/system/oss/oss.js
+++ quanmin_admin/main/src/main/resources/static/js/system/oss/oss.js
@@ -13,8 +13,7 @@ $(function() {
                     colkey : "url",
                     name : "图片",
                     renderData : function(rowindex, data, rowdata, colkeyn) {
-                        
-                        return "<img src='"+data+"'>";
+                        return "<img width='300px' hight='300px' src='" + data + "'>";
                     }
                 }, {
                     colkey : "createdate",
@@ -23,15 +22,35 @@ $(function() {
         jsonUrl : rootPath + '/sys/oss_do/list.do',
         checkbox : true
     });
-//    $("#search").click("click", function() {// 绑定查询按扭
-//        var searchParams = $("#searchForm").serializeJson();// 初始化传参数
-//        grid.setOptions({
-//            data : searchParams
-//        });
-//    });
     
-   
+    /****/
+    
+    
+    
+    // $("#search").click("click", function() {// 绑定查询按扭
+    // var searchParams = $("#searchForm").serializeJson();// 初始化传参数
+    // grid.setOptions({
+    // data : searchParams
+    // });
+    // });
 });
 
-
-
+function del(){
+    var cbox = grid.getSelectedCheckbox();
+    if (cbox == "") {
+        layer.msg("请选择删除项！！");
+        return;
+    }
+    layer.confirm('是否删除？', function(index) {
+        var url = rootPath + "/sys/oss_do/delete.do";
+        var s = CommnUtil.ajax(url, {
+            ids : cbox.join(",")
+        }, "json");
+        if (s.success ) {
+            alert('删除成功');
+            grid.loadData();
+        } else {
+            alert('删除失败');
+        }
+    });
+}
