b43463373a8c16d4fefd9c9f0922c4525945977f
Deament <591518884@qq.com>
2017-07-11 21:44:27 +0800

    no commit message

--- quanmin_admin/ybg_gen/src/main/java/com/ybg/gen/controller/SysGeneratorController.java
+++ quanmin_admin/ybg_gen/src/main/java/com/ybg/gen/controller/SysGeneratorController.java
@@ -3,8 +3,13 @@ import com.ybg.base.util.Page;
 import com.ybg.gen.qvo.GeneratorQuery;
 import com.ybg.gen.service.SysGeneratorService;
 import com.ybg.gen.utils.xss.XssHttpServletRequestWrapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,6 +26,7 @@ import java.io.IOException;
  * @author chenshun
  * @email sunlightcs@gmail.com
  * @date 2016年12月19日 下午9:12:58 */
+@Api("代码生成器")
 @Controller
 @RequestMapping("/sys/generator_do/")
 public class SysGeneratorController {
@@ -28,13 +34,16 @@ public class SysGeneratorController {
 	@Autowired
 	private SysGeneratorService sysGeneratorService;
 	
-	@RequestMapping(value = "index.do", method = { RequestMethod.GET, RequestMethod.POST })
+	@ApiOperation(value = "代码生成器首页", notes = "", produces = MediaType.TEXT_HTML_VALUE)
+	@RequestMapping(value = { "index.do" }, method = { RequestMethod.GET, RequestMethod.POST })
 	public String index() {
 		return "/gen/index";
 	}
 	
+	@ApiOperation(value = "代码生成器列表", notes = "", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiImplicitParams({ @ApiImplicitParam(name = "pageNow", value = "当前页数", required = true, dataType = "Integer"), @ApiImplicitParam(name = "qvo", value = "查询页数", required = false, dataType = "GeneratorQuery") })
 	@ResponseBody
-	@RequestMapping(value = "list.do", method = { RequestMethod.GET, RequestMethod.POST })
+	@RequestMapping(value = { "list.do" }, method = { RequestMethod.GET, RequestMethod.POST })
 	public Page list(@ModelAttribute GeneratorQuery qvo, @RequestParam(name = "pageNow", required = false, defaultValue = "0") Integer pageNow, ModelMap map) throws Exception {
 		Page page = new Page();
 		page.setCurPage(pageNow);
@@ -44,7 +53,9 @@ public class SysGeneratorController {
 	}
 	
 	/** 生成代码 */
-	@RequestMapping("code.do")
+	@ApiOperation(value = "生成代码", notes = "只能页面传输，参数是tables 用英文逗号分隔", produces = MediaType.APPLICATION_JSON_VALUE)
+	
+	@RequestMapping(value = { "code.do" }, method = { RequestMethod.GET, RequestMethod.POST })
 	public void code(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String[] tableNames = new String[] {};
 		// 获取表名，不进行xss过滤
@@ -53,7 +64,7 @@ public class SysGeneratorController {
 		tableNames = tables.split(",");
 		byte[] data = sysGeneratorService.generatorCode(tableNames);
 		response.reset();
-		response.setHeader("Content-Disposition", "attachment; filename=\"renren.zip\"");
+		response.setHeader("Content-Disposition", "attachment; filename=\"gencode.zip\"");
 		response.addHeader("Content-Length", "" + data.length);
 		response.setContentType("application/octet-stream; charset=UTF-8");
 		IOUtils.write(data, response.getOutputStream());
