a45dbc216d7db1ece45cb48eb200d67a1b8d1b70
Deament <591518884@qq.com>
2017-07-25 21:10:32 +0800

    多数据源配置

---
+++ quanmin_admin/ybg_base_jdbc/src/main/java/com/ybg/base/jdbc/DataBaseConstant.java
@@ -0,0 +1,11 @@
+package com.ybg.base.jdbc;
+
+
+public class DataBaseConstant {
+	public static final String DB_SYS="dataSourceSys";
+	public static final String DB_OA="dataSourceOa";
+	public static final String DB_EDU="dataSourceEdu";
+	public static final String JD_SYS="sysJdbcTemplate";
+	public static final String JD_OA="oaJdbcTemplate";
+	public static final String JD_EDU="eduJdbcTemplate";
+}
