88b7a2a3c2e2ea976915349bd04e6a476445ae13
Deament <591518884@qq.com>
2017-07-29 05:43:41 +0800

    添加注释

--- quanmin_admin/ybg_base_jdbc/src/main/java/com/ybg/base/jdbc/DataBaseConstant.java
+++ quanmin_admin/ybg_base_jdbc/src/main/java/com/ybg/base/jdbc/DataBaseConstant.java
@@ -1,11 +1,18 @@
 package com.ybg.base.jdbc;
-
-
+/** 数据库常量 **/
 public class DataBaseConstant {
-	public static final String DB_SYS="dataSourceSys";
-	public static final String DB_OA="dataSourceOa";
-	public static final String DB_EDU="dataSourceEdu";
-	public static final String JD_SYS="sysJdbcTemplate";
-	public static final String JD_OA="oaJdbcTemplate";
-	public static final String JD_EDU="eduJdbcTemplate";
+	
+	/** 数据源-系统管理 (默认)**/
+	public static final String	DB_SYS	= "dataSourceSys";
+	/** 数据源-企业管理 **/
+	public static final String	DB_OA	= "dataSourceOa";
+	/** 数据源-教育管理 **/
+	public static final String	DB_EDU	= "dataSourceEdu";
+	// 分隔线-----------------------------------------------
+	/** JDBC模板方法-使用系统数据库  (默认)**/
+	public static final String	JD_SYS	= "sysJdbcTemplate";
+	/** JDBC模板方法-使用OA数据库- **/
+	public static final String	JD_OA	= "oaJdbcTemplate";
+	/** JDBC模板方法-使用教育数据库 **/
+	public static final String	JD_EDU	= "eduJdbcTemplate";
 }
