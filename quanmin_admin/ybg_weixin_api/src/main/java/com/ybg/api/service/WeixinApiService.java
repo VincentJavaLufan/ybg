6db13a8e5fafaa83b3bf519b1f40bfb95a80024a
Deament <591518884@qq.com>
2017-08-17 22:47:27 +0800

    no commit message

--- quanmin_admin/ybg_weixin_api/src/main/java/com/ybg/api/service/WeixinApiService.java
+++ quanmin_admin/ybg_weixin_api/src/main/java/com/ybg/api/service/WeixinApiService.java
@@ -5,6 +5,6 @@ public interface WeixinApiService {
 	
 	Map<String, String> getSetting();
 	
-	/**根据CODE 获取OPEN_ID  没有或报错 就返回nukk**/
+	/**根据CODE 获取OPEN_ID  没有或报错 就返回null**/
 	String getOpenidByCode(String code);
 }
