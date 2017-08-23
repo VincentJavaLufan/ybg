f4d3118755f50949fcd2b5703ccb69fa3d3b8296
Deament <591518884@qq.com>
2017-08-24 07:37:02 +0800

    工作流界面（未完成）

---
+++ quanmin_admin/main/src/main/resources/static/js/activiti/activiti.js
@@ -0,0 +1,100 @@
+var vm = new Vue({
+    el : '#rrapp',
+    data : {
+        showList : true,
+        title : null,
+        m : {}
+    },
+    methods : {
+        query : function() {
+            vm.reload();
+        },
+        add : function() {
+            vm.showList = false;
+            vm.title = "新增";
+            vm.m = {};
+        },
+        update : function() {
+            var jobId = getSelectedRow();
+            if (jobId == null) {
+                return;
+            }
+            $.get(rootPath + "/models/model_do/info.do?jobId=" + jobId, function(r) {
+                vm.showList = false;
+                vm.title = "修改";
+                vm.m = r.m;
+            });
+        },
+        saveOrUpdate : function(event) {
+            var url = vm.m.id == null ? rootPath + '/models/model_do/create.do' : rootPath + '/models/model_do/update.do';
+            $.ajax({
+                type : "POST",
+                url : rootPath + url,
+                contentType : "application/json",
+                data : JSON.stringify(vm.m),
+                success : function(r) {
+                    vm.reload();
+                    alert(r.msg);
+                }
+            });
+        },
+        del : function() {
+            var jobIds = getSelectedRows();
+            if (jobIds == null) {
+                return;
+            }
+            confirm('确定要删除选中的记录？', function() {
+                $.ajax({
+                    type : "POST",
+                    url : rootPath + "/models/model_do/delete.do",
+                    // contentType : "application/json",
+                    data : {
+                        jobIds : jobIds
+                    },
+                    success : function(r) {
+                        vm.reload();
+                        alert(r.msg);
+                    }
+                });
+            });
+        },
+        reload : function(event) {
+            vm.showList = true;
+            grid.loadData();
+        }
+    }
+});
+var pageii = null;
+var grid = null;
+$(function() {
+    grid = lyGrid({
+        id : 'paging',
+        l_column : [
+                {
+                    colkey : "id",
+                    name : "ID",
+                    width : "50px",
+                    hide : true
+                }, {
+                    colkey : "name",
+                    name : "流程名称"
+                }, {
+                    colkey : "key",
+                    name : "流程关键字",
+                }, {
+                    colkey : "createTime",
+                    name : "创建时间",
+                }, {
+                    colkey : "id",
+                    name : "操作",
+                    renderData : function(rowindex, data, rowdata, colkey)// 渲染数据
+                    {
+                        return "<a target='_blank' class='btn btn-primary' href='/modeler.html?modelId=" + rowdata.id + "'> <i class='fa fa-pencil-square-o'></i>设计流程</a>";
+                    }
+                }],
+        jsonUrl : rootPath + '/models/model_do/list.do',
+        usePage : false,
+        checkbox : true,
+        checkValue : 'id',
+    });
+});
--- quanmin_admin/ybg_activiti/src/main/java/com/ybg/activiti/rest/editor/controller/ModelCreateController.java
+++ quanmin_admin/ybg_activiti/src/main/java/com/ybg/activiti/rest/editor/controller/ModelCreateController.java
@@ -2,21 +2,28 @@ package com.ybg.activiti.rest.editor.controller;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.ybg.base.jdbc.util.QvoConditionUtil;
+import com.ybg.base.util.Json;
+import com.ybg.base.util.Page;
+import io.swagger.annotations.Api;
 import org.activiti.bpmn.converter.BpmnXMLConverter;
 import org.activiti.bpmn.model.BpmnModel;
 import org.activiti.editor.constants.ModelDataJsonConstants;
 import org.activiti.editor.language.json.converter.BpmnJsonConverter;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.impl.persistence.entity.ModelEntity;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.Model;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 /** 懒得写，直接参照了：http://www.jianshu.com/p/cf766a713a86 自己增加的controller，用于对model进行操作，返回值随便弄的，需要修改 Created by chenhai on 2017/6/6. */
-@RestController
+@Api(tags = "工作流-模型API")
+@Controller
 @RequestMapping("/models/model_do/")
 public class ModelCreateController {
 	
@@ -25,25 +32,43 @@ public class ModelCreateController {
 	@Autowired
 	ObjectMapper	objectMapper;
 	
+	/*** 首页 **/
+	@GetMapping(value = { "index.do" })
+	public String index() {
+		return "/activiti/activiti";
+	}
+	
 	/** 新建一个空模型
 	 * 
 	 * @return
 	 * @throws UnsupportedEncodingException */
-	@GetMapping("newModel.do")
-	public String newModel() throws UnsupportedEncodingException {
+	@ResponseBody
+	@PostMapping("create.do")
+	public Json newModel(@RequestBody ModelEntity m) throws UnsupportedEncodingException {
+		Json json = new Json();
+		if (!QvoConditionUtil.checkString(m.getName())) {
+			json.setSuccess(false);
+			json.setMsg("操作失败，名称为空");
+			return json;
+		}
+		if (!QvoConditionUtil.checkString(m.getKey())) {
+			json.setSuccess(false);
+			json.setMsg("操作失败，关键字为空");
+			return json;
+		}
 		RepositoryService repositoryService = processEngine.getRepositoryService();
 		// 初始化一个空模型
 		Model model = repositoryService.newModel();
 		// 设置一些默认信息
-		String name = "new-process";
+		// String name = "new-process";
 		String description = "";
 		int revision = 1;
-		String key = "process";
+		String key = m.getKey();
 		ObjectNode modelNode = objectMapper.createObjectNode();
-		modelNode.put(ModelDataJsonConstants.MODEL_NAME, name);
+		modelNode.put(ModelDataJsonConstants.MODEL_NAME, m.getName());
 		modelNode.put(ModelDataJsonConstants.MODEL_DESCRIPTION, description);
 		modelNode.put(ModelDataJsonConstants.MODEL_REVISION, revision);
-		model.setName(name);
+		model.setName(m.getName());
 		model.setKey(key);
 		model.setMetaInfo(modelNode.toString());
 		repositoryService.saveModel(model);
@@ -56,55 +81,63 @@ public class ModelCreateController {
 		stencilSetNode.put("namespace", "http://b3mn.org/stencilset/bpmn2.0#");
 		editorNode.put("stencilset", stencilSetNode);
 		repositoryService.addModelEditorSource(id, editorNode.toString().getBytes("utf-8"));
-		return "success";
+		json.setSuccess(true);
+		json.setMsg("操作成功");
+		return json;
 	}
 	
-//	/** 获取所有模型
-//	 * 
-//	 * @return */
-//	@GetMapping("modelList")
-//	public List<Model> modelList() {
-//		RepositoryService repositoryService = processEngine.getRepositoryService();
-//		List<Model> models = repositoryService.createModelQuery().list();
-//		return models;
-//	}
-//	
-//	/** 删除模型
-//	 * 
-//	 * @param id
-//	 * @return */
-//	@DeleteMapping("{id}")
-//	public String deleteModel(@PathVariable("id") String id) {
-//		RepositoryService repositoryService = processEngine.getRepositoryService();
-//		repositoryService.deleteModel(id);
-//		return "success";
-//	}
-//	
-//	/** 发布模型为流程定义
-//	 * 
-//	 * @param id
-//	 * @return
-//	 * @throws Exception */
-//	@PostMapping("{id}/deployment")
-//	public String deploy(@PathVariable("id") String id) throws Exception {
-//		// 获取模型
-//		RepositoryService repositoryService = processEngine.getRepositoryService();
-//		Model modelData = repositoryService.getModel(id);
-//		byte[] bytes = repositoryService.getModelEditorSource(modelData.getId());
-//		if (bytes == null) {
-//			return ("模型数据为空，请先设计流程并成功保存，再进行发布。");
-//		}
-//		JsonNode modelNode = new ObjectMapper().readTree(bytes);
-//		BpmnModel model = new BpmnJsonConverter().convertToBpmnModel(modelNode);
-//		if (model.getProcesses().size() == 0) {
-//			return ("数据模型不符要求，请至少设计一条主线流程。");
-//		}
-//		byte[] bpmnBytes = new BpmnXMLConverter().convertToXML(model);
-//		// 发布流程
-//		String processName = modelData.getName() + ".bpmn20.xml";
-//		Deployment deployment = repositoryService.createDeployment().name(modelData.getName()).addString(processName, new String(bpmnBytes, "UTF-8")).deploy();
-//		modelData.setDeploymentId(deployment.getId());
-//		repositoryService.saveModel(modelData);
-//		return "success";
-//	}
+	/** 获取所有模型
+	 * 
+	 * @return */
+	@ResponseBody
+	@PostMapping("list.do")
+	public Page modelList() {
+		RepositoryService repositoryService = processEngine.getRepositoryService();
+		List<Model> models = repositoryService.createModelQuery().list();
+		Page page = new Page();
+		page.setCurPage(1);
+		page.setResult(models);
+		page.setTotals(models.size());
+		page.init();
+		return page;
+	}
+	//
+	// /** 删除模型
+	// *
+	// * @param id
+	// * @return */
+	// @DeleteMapping("{id}")
+	// public String deleteModel(@PathVariable("id") String id) {
+	// RepositoryService repositoryService = processEngine.getRepositoryService();
+	// repositoryService.deleteModel(id);
+	// return "success";
+	// }
+	//
+	// /** 发布模型为流程定义
+	// *
+	// * @param id
+	// * @return
+	// * @throws Exception */
+	// @PostMapping("{id}/deployment")
+	// public String deploy(@PathVariable("id") String id) throws Exception {
+	// // 获取模型
+	// RepositoryService repositoryService = processEngine.getRepositoryService();
+	// Model modelData = repositoryService.getModel(id);
+	// byte[] bytes = repositoryService.getModelEditorSource(modelData.getId());
+	// if (bytes == null) {
+	// return ("模型数据为空，请先设计流程并成功保存，再进行发布。");
+	// }
+	// JsonNode modelNode = new ObjectMapper().readTree(bytes);
+	// BpmnModel model = new BpmnJsonConverter().convertToBpmnModel(modelNode);
+	// if (model.getProcesses().size() == 0) {
+	// return ("数据模型不符要求，请至少设计一条主线流程。");
+	// }
+	// byte[] bpmnBytes = new BpmnXMLConverter().convertToXML(model);
+	// // 发布流程
+	// String processName = modelData.getName() + ".bpmn20.xml";
+	// Deployment deployment = repositoryService.createDeployment().name(modelData.getName()).addString(processName, new String(bpmnBytes, "UTF-8")).deploy();
+	// modelData.setDeploymentId(deployment.getId());
+	// repositoryService.saveModel(modelData);
+	// return "success";
+	// }
 }
