f84a7abcd5e83f6aef35462afc564830098bac76
Deament <591518884@qq.com>
2017-09-04 22:50:13 +0800

    no commit message

--- quanmin_admin/ybg_weixin_api/src/main/java/com/ybg/tags/controller/WeixinTagsController.java
+++ quanmin_admin/ybg_weixin_api/src/main/java/com/ybg/tags/controller/WeixinTagsController.java
@@ -1,4 +1,6 @@
 package com.ybg.tags.controller;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +9,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.ybg.base.util.Json;
 import com.ybg.base.util.Page;
 import com.ybg.tags.domain.WeixinTagsVO;
+import com.ybg.tags.service.WeixinTagsService;
 import io.swagger.annotations.Api;
 
 @Api(tags = "微信用户标签API")
 @Controller
 @RequestMapping("/weixin/tags/")
 public class WeixinTagsController {
+	
+	@Autowired
+	WeixinTagsService weixinTagsService;
 	
 	@RequestMapping(value = { "index.do" }, method = { RequestMethod.GET, RequestMethod.POST })
 	public String index() {
@@ -23,6 +29,11 @@ public class WeixinTagsController {
 	@RequestMapping(value = { "list.do" }, method = { RequestMethod.GET, RequestMethod.POST })
 	public Page list() {
 		Page page = new Page();
+		List<WeixinTagsVO> list = weixinTagsService.list();
+		page.setCurPage(1);
+		page.setResult(list);
+		page.setTotals(list.size());
+		page.init();
 		return page;
 	}
 	
@@ -30,27 +41,36 @@ public class WeixinTagsController {
 	@RequestMapping(value = { "create.do" }, method = { RequestMethod.GET, RequestMethod.POST })
 	public Json create(@RequestBody WeixinTagsVO tags) {
 		Json j = new Json();
+		try {
+			weixinTagsService.create(tags);
+		} catch (Exception e) {
+			j.setMsg("操作失败");
+			j.setSuccess(false);
+			return j;
+		}
 		j.setMsg("操作成功");
 		j.setSuccess(true);
 		return j;
 	}
-	//更需必须要 id和name
+	
+	// 更需必须要 id和name
 	@ResponseBody
 	@RequestMapping(value = { "update.do" }, method = { RequestMethod.GET, RequestMethod.POST })
 	public Json update(@RequestBody WeixinTagsVO tags) {
 		Json j = new Json();
-		
-		
-		
+		weixinTagsService.update(tags.getId(), tags.getName());
 		j.setMsg("操作成功");
 		j.setSuccess(true);
 		return j;
 	}
-	//根据ID 删除
+	
+	// 根据ID 删除
 	@ResponseBody
 	@RequestMapping(value = { "remove.do" }, method = { RequestMethod.GET, RequestMethod.POST })
 	public Json remove(Integer id) {
 		Json j = new Json();
+		
+		weixinTagsService.remove(id);
 		j.setMsg("操作成功");
 		j.setSuccess(true);
 		return j;
@@ -59,8 +79,9 @@ public class WeixinTagsController {
 	// 批量为用户取消标签
 	@ResponseBody
 	@RequestMapping(value = { "batchuntagging.do" }, method = { RequestMethod.GET, RequestMethod.POST })
-	public Json batchuntagging(String openids,Integer tagid) {
+	public Json batchuntagging(String openids, Integer tagid) {
 		Json j = new Json();
+		// weixinTagsService.batchuntagging(openids, tagid);
 		j.setMsg("操作成功");
 		j.setSuccess(true);
 		return j;
@@ -69,7 +90,7 @@ public class WeixinTagsController {
 	// 批量为用户打上标签
 	@ResponseBody
 	@RequestMapping(value = { "batchtagging.do" }, method = { RequestMethod.GET, RequestMethod.POST })
-	public Json batchtagging(String openids,Integer tagid) {
+	public Json batchtagging(String openids, Integer tagid) {
 		Json j = new Json();
 		j.setMsg("操作成功");
 		j.setSuccess(true);
