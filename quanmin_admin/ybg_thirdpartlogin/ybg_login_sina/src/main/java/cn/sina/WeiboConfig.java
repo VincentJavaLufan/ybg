62e0313cc92a94bee9ac05b1b19811659f51c6e5
Deament <591518884@qq.com>
2017-10-07 19:06:52 +0800

    1.4 版本 引入了 spring session 和 spring social

---
+++ quanmin_admin/ybg_thirdpartlogin/ybg_login_sina/src/main/java/cn/sina/WeiboConfig.java
@@ -0,0 +1,32 @@
+package cn.sina;
+import java.util.Map;
+import java.util.Properties;
+import com.ybg.base.util.ReplaceDomainUtil;
+import com.ybg.base.util.SpringContextUtils;
+import cn.sina.service.WeiboUserService;
+
+public class WeiboConfig {
+	
+	public static final String redirect_URI="redirect_URI";
+	public WeiboConfig() {
+	}
+	
+	private static Properties props = new Properties();
+	static {
+		reflushProperties();
+	}
+	
+	public static String getValue(String key) {
+		return props.getProperty(key);
+	}
+	
+	public static void updateProperties(String key, String value) {
+		props.setProperty(key, value);
+	}
+	public static void reflushProperties() {
+		WeiboUserService service = (WeiboUserService) SpringContextUtils.getBean(WeiboUserService.class);
+		Map<String, String> map=	service.getSetting();
+	//	map.put("redirect_URI", ReplaceDomainUtil.replacedomain(map.get(redirect_URI)) );
+		props.putAll(map);
+	}
+}
--- quanmin_admin/ybg_thirdpartlogin/ybg_login_sina/src/main/java/cn/sina/service/WeiboUserServiceImpl.java
+++ quanmin_admin/ybg_thirdpartlogin/ybg_login_sina/src/main/java/cn/sina/service/WeiboUserServiceImpl.java
@@ -1,42 +1,14 @@
 package cn.sina.service;
-import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import com.ybg.base.jdbc.BaseMap;
-import com.ybg.base.jdbc.util.QvoConditionUtil;
 import cn.sina.dao.WeiboUserDao;
-import cn.sina.domain.WeiboUserVO;
-import cn.sina.qvo.WeiboUserQuery;
 
 @Repository
 public class WeiboUserServiceImpl implements WeiboUserService {
 	
 	@Autowired
 	WeiboUserDao weiboUserDao;
-	
-	public void create(WeiboUserVO bean) throws Exception {
-		weiboUserDao.create(bean);
-	}
-	
-	public void update(BaseMap<String, Object> updatemap, BaseMap<String, Object> wheremap) {
-		weiboUserDao.update(updatemap, wheremap);
-	}
-	
-	public void remove(BaseMap<String, Object> wheremap) {
-		weiboUserDao.remove(wheremap);
-	}
-	
-	public List<WeiboUserVO> query(WeiboUserQuery qvo) throws Exception {
-		return weiboUserDao.query(qvo);
-	}
-	
-	public WeiboUserVO get(String uid) throws Exception {
-		WeiboUserQuery qvo = new WeiboUserQuery();
-		qvo.setUid(uid);
-		List<WeiboUserVO> list = weiboUserDao.query(qvo);
-		return list != null && list.size() > 0 ? list.get(0) : null;
-	}
 	
 	public Map<String, String> getSetting() {
 		return weiboUserDao.getSetting();
@@ -45,13 +17,5 @@ public class WeiboUserServiceImpl implements WeiboUserService {
 	@Override
 	public void updateSetting(String appid, String value, String url) {
 		weiboUserDao.updateSetting(appid, value, url);
-	}
-	
-	@Override
-	public String queryWeiboId(String userid) {
-		if (!QvoConditionUtil.checkString(userid)) {
-			return "";
-		}
-		return weiboUserDao.queryWeiboId(userid);
 	}
 }
