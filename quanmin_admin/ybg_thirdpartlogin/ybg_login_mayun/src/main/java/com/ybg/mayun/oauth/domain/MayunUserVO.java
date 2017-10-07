a6968fb08f5dec7348476c97c2953108ac064c83
Deament <591518884@qq.com>
2017-10-07 19:00:16 +0800

    1.4 版本 引入了 spring session 和 spring social

--- quanmin_admin/ybg_thirdpartlogin/ybg_login_mayun/src/main/java/com/ybg/mayun/oauth/domain/MayunUserVO.java
+++
@@ -1,5 +0,0 @@
-package com.ybg.mayun.oauth.domain;
-
-
-public class MayunUserVO extends MayunUserDO{
-}
--- quanmin_admin/ybg_thirdpartlogin/ybg_login_mayun/src/main/java/com/ybg/mayun/oauth/qvo/MayunUserQuery.java
+++
@@ -1,6 +0,0 @@
-package com.ybg.mayun.oauth.qvo;
-
-import com.ybg.mayun.oauth.domain.MayunUserVO;
-
-public class MayunUserQuery extends MayunUserVO {
-}
