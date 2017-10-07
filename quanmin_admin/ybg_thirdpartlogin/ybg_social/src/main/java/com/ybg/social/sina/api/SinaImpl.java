734af13b6d73e50c20c1b721a5b81c85ac4d1f5a
Deament <591518884@qq.com>
2017-10-07 13:13:53 +0800

    no commit message

--- quanmin_admin/ybg_thirdpartlogin/ybg_social/src/main/java/com/ybg/social/sina/api/SinaImpl.java
+++ quanmin_admin/ybg_thirdpartlogin/ybg_social/src/main/java/com/ybg/social/sina/api/SinaImpl.java
@@ -4,34 +4,30 @@ import org.springframework.social.oauth2.AbstractOAuth2ApiBinding;
 import org.springframework.social.oauth2.TokenStrategy;
 import com.alibaba.fastjson.JSONObject;
 import com.xiaoleilu.hutool.http.HttpUtil;
+
 //如果跳转到了  /signin页面 则表示 此类有问题。。真的是这个类
 public class SinaImpl extends AbstractOAuth2ApiBinding implements Sina {
-	//根据 TOKEN 换取 用户ID 的地址。
+	
+	// 根据 TOKEN 换取 用户ID 的地址。
 	private static final String	URL_GET_OPENID		= "https://api.weibo.com/oauth2/get_token_info";
-	//根据用户ID 请求 用户详细信息的地址
+	// 根据用户ID 请求 用户详细信息的地址
 	private static final String	URL_GET_USERINFO	= "https://api.weibo.com/2/users/show.json";
 	private String				appId;
 	private String				id;
-	private String  accessToken;
+	private String				accessToken;
+	
 	public SinaImpl(String accessToken, String appId) {
 		super(accessToken, TokenStrategy.ACCESS_TOKEN_PARAMETER);
-		System.out.println("16:SinaImpl");
 		this.appId = appId;
-		this.accessToken=accessToken;
-		String result = getRestTemplate().postForObject(URL_GET_OPENID,null, String.class);
-		System.out.println(result+":20SinaImpl");
+		this.accessToken = accessToken;
+		String result = getRestTemplate().postForObject(URL_GET_OPENID, null, String.class);
 		this.id = new JSONObject().parseObject(result, Map.class).get("uid").toString();
-		System.out.println("22:SinaImpl"+id);
 	}
 	
 	@Override
 	public SinaUserInfo getUserInfo() {
-		System.out.println("28:" + URL_GET_USERINFO+"?access_token="+accessToken+"&uid="+this.id);
-		String result = HttpUtil.get(URL_GET_USERINFO+"?access_token="+accessToken+"&uid="+this.id);
-		System.out.println("28:" + result);
+		String result = HttpUtil.get(URL_GET_USERINFO + "?access_token=" + accessToken + "&uid=" + this.id);
 		SinaUserInfo bean = JSONObject.parseObject(result, SinaUserInfo.class);
-		
-		
 		return bean;
 	}
 }
