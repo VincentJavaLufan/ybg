2f6a590f6ae4a7adabfbc0567f0405dc5e6e068e
Deament <591518884@qq.com>
2017-10-09 23:04:13 +0800

    1.5 初版

--- quanmin_admin/main/src/main/java/App.java
+++ quanmin_admin/main/src/main/java/App.java
@@ -7,21 +7,16 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 
 //@EnableAutoConfiguration    //这个注解可以根据你依赖的包自动生成相关配置
 @ComponentScan(basePackages = { "org.activiti.rest.diagram", "com.ybg", "cn", "org.mybatis" }) // 扫描注解
-@EnableAutoConfiguration(exclude = {
-org.activiti.spring.boot.SecurityAutoConfiguration.class,
-org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration.class
-})
-@EnableAdminServer
-public class App extends SpringBootServletInitializer
-{  
-    public static void main( String[] args )  
-    {  
-        SpringApplication.run(App.class, args);  
-    }  
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        // TODO Auto-generated method stub
-        return builder.sources(App.class);
-    }
-} 
+@EnableAutoConfiguration(exclude = { org.activiti.spring.boot.SecurityAutoConfiguration.class, org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration.class })
+@EnableAdminServer // spring boot admin监控 ，不喜欢可以不要
+public class App extends SpringBootServletInitializer {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(App.class);
+	}
+}
