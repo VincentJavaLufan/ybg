20af604ec77f36ef3610fce78ba08bc301ad8e09
Deament <591518884@qq.com>
2017-10-19 07:26:01 +0800

    no commit message

--- ybg_urule_server/src/main/java/com/ybg/urule/server/UruleConfiguration.java
+++ ybg_urule_server/src/main/java/com/ybg/urule/server/UruleConfiguration.java
@@ -1,4 +1,5 @@
 package com.ybg.urule.server;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -7,14 +8,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import com.bstek.urule.console.servlet.URuleServlet;
 
+/**
+ * @author Deament 相当于 configure.properties
+ * @since 2017年10月12日
+ */
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-@ImportResource({"classpath:urule-console-context.xml"})
-public class URuleConfiguration {
-	
-	@Bean(name="uRuleServlet")
+@ImportResource({ "classpath:urule-console-context.xml" })
+public class UruleConfiguration {
+
+	@Bean(name = "uRuleServlet")
 	public ServletRegistrationBean registerURuleServlet() {
 		return new ServletRegistrationBean(new URuleServlet(), "/urule/*");
 	}
