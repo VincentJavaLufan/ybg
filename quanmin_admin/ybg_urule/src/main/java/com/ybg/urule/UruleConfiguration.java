978684b5c8eccee26eae9b3f2077a8f64ce638f8
Deament <591518884@qq.com>
2017-10-19 07:23:54 +0800

    urule 服务端 打包

--- quanmin_admin/ybg_urule/src/main/java/com/ybg/urule/UruleConfiguration.java
+++ quanmin_admin/ybg_urule/src/main/java/com/ybg/urule/UruleConfiguration.java
@@ -1,22 +1,21 @@
 package com.ybg.urule;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import com.bstek.urule.KnowledgePackageReceiverServlet;
 
-
+/**
+ * @author Deament
+ * @since 2017年10月19日
+ **/
 
 @Configuration
-//@ComponentScan
-//@EnableAutoConfiguration
-@ImportResource({"classpath:urule-core-context.xml"})
-public class URuleConfiguration {
-	
+@ImportResource({ "classpath:urule-core-context.xml" })
+public class UruleConfiguration {
 
-	@Bean(name="uRuleServlet")
+	@Bean(name = "uRuleServlet")
 	public ServletRegistrationBean registerURuleServlet() {
 		return new ServletRegistrationBean(new KnowledgePackageReceiverServlet(), "/knowledgepackagereceiver");
 	}
