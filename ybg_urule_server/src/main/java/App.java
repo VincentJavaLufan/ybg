0739e4c2b2a667379fcd3d190724121f07df4f37
Deament <591518884@qq.com>
2017-10-20 22:18:30 +0800

    阿里巴巴规范

--- ybg_urule_server/src/main/java/App.java
+++ ybg_urule_server/src/main/java/App.java
@@ -5,11 +5,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
+/*** @author https://gitee.com/YYDeament/88ybg
+ * 
+ * @date 2017/10/1 */
 @SpringBootApplication
-@EnableAutoConfiguration    //这个注解可以根据你依赖的包自动生成相关配置
+@EnableAutoConfiguration // 这个注解可以根据你依赖的包自动生成相关配置
 @ComponentScan(basePackages = { "com.bstek", "com.ybg", "cn", "org.mybatis" }) // 扫描注解
-//@EnableAutoConfiguration(exclude = { org.activiti.spring.boot.SecurityAutoConfiguration.class, org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration.class })
-//@EnableAdminServer // spring boot admin监控 ，不喜欢可以不要
+// @EnableAutoConfiguration(exclude = { org.activiti.spring.boot.SecurityAutoConfiguration.class, org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration.class })
+// @EnableAdminServer // spring boot admin监控 ，不喜欢可以不要
 public class App extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
