eab555b6bf012d1e0ed2101e8412e54a62de7a74
Deament <591518884@qq.com>
2017-10-20 23:28:05 +0800

    阿里巴巴规范

--- quanmin_admin/ybg_inter/src/main/java/com/ybg/component/org/inter/Organization.java
+++ quanmin_admin/ybg_inter/src/main/java/com/ybg/component/org/inter/Organization.java
@@ -1,11 +1,24 @@
 package com.ybg.component.org.inter;
-/** 组织接口 **/
+/** * @author https://gitee.com/YYDeament/88ybg
+ * 
+ * 
+ * @date 2016/10/1组织接口 **/
 public interface Organization {
 	
-	/** 是否子节点 **/
+	/** 是否子节点
+	 * 
+	 * @return */
 	boolean isLeaf();
 	
+	/** 创建组织
+	 * 
+	 * @param org
+	 */
 	void add(Organization org);
 	
+	/** 删除组织
+	 * 
+	 * @param org
+	 */
 	void remove(Organization org);
 }
