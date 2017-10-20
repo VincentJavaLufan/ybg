141f4b4d21e46dc9c27b763c82f362eb7b355b1d
Deament <591518884@qq.com>
2017-10-20 23:44:47 +0800

    阿里巴巴规范

--- quanmin_admin/ybg_gen/src/main/java/com/ybg/gen/utils/utils/HttpContextUtils.java
+++ quanmin_admin/ybg_gen/src/main/java/com/ybg/gen/utils/utils/HttpContextUtils.java
@@ -1,12 +1,13 @@
 package com.ybg.gen.utils.utils;
-
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
 import javax.servlet.http.HttpServletRequest;
 
+/*** @author https://gitee.com/YYDeament/88ybg
+ * 
+ * @date 2016/10/1 */
 public class HttpContextUtils {
-
+	
 	public static HttpServletRequest getHttpServletRequest() {
 		return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
 	}
