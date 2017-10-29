c28cbb2670dfa08a37a2b5be1d6428c151639d41
Deament <591518884@qq.com>
2017-10-29 08:17:15 +0800

    本地文件上传

---
+++ quanmin_admin/ybg_local_upload/src/main/java/com/ybg/upload/configuration/FileUploadConfiguration.java
@@ -0,0 +1,28 @@
+package com.ybg.upload.configuration;
+
+import javax.servlet.MultipartConfigElement;
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * 文件上传配置
+ * 
+ * @author 单红宇(CSDN CATOOP)
+ * @create 2017年3月11日
+ */
+public class FileUploadConfiguration {
+
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		// 设置文件大小限制 ,超出设置页面会抛出异常信息，
+		// 这样在文件上传的地方就需要进行异常信息的处理了;
+		factory.setMaxFileSize("256KB"); // KB,MB
+		/// 设置总上传数据总大小
+		factory.setMaxRequestSize("512KB");
+		// Sets the directory location where files will be stored.
+		// factory.setLocation("路径地址");
+		return factory.createMultipartConfig();
+	}
+}
