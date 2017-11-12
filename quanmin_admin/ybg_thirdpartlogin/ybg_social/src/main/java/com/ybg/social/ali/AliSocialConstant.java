53ab7f7629c13574df794b9d913e06cd7430b3c0
Administrator <Administrator@E0DAE2755PWM0QB>
2017-11-12 10:50:12 +0800

    常量

---
+++ quanmin_admin/ybg_thirdpartlogin/ybg_social/src/main/java/com/ybg/social/ali/AliSocialConstant.java
@@ -0,0 +1,13 @@
+package com.ybg.social.ali;
+
+/**
+ * @author https://gitee.com/YYDeament/88ybg
+ *
+ * @date
+ * @version v1.0
+ */
+public class AliSocialConstant {
+
+	/** 是否沙箱环境 如果非测试环境 把dev 改成"" **/
+	public static final String DEV = "dev";
+}
