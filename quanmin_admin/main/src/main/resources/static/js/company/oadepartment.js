d38483e664f37bc3c2580b9914689d87178a0e8b
Deament <591518884@qq.com>
2018-02-01 20:46:49 +0800

    no commit message

--- quanmin_admin/main/src/main/resources/static/js/company/oadepartment.js
+++ quanmin_admin/main/src/main/resources/static/js/company/oadepartment.js
@@ -1,3 +1,17 @@
+var setting = {
+    data : {
+        simpleData : {
+            enable : true,
+            idKey : "id",
+            pIdKey : "parentid",
+            rootPId : "0"
+        },
+        key : {
+            url : "nourl"
+        }
+    }
+};
+var ztree;
 var vm = new Vue({
     el : '#rrapp',
     data : {
@@ -61,123 +75,125 @@ var vm = new Vue({
         },
         reload : function(event) {
             vm.showList = true;
-            layui.use('table', function() {
-                var table = layui.table;
-                table.render({
-                    elem : '#oaDepartmenttable' // 选定是那个DIV
-                    ,
-                    url : rootPath + '/oa/department_do/list.do',
-                    cols : [
-                        [
-                            {
-                                type : 'checkbox'
-                            }, {
-                                field : 'id',
-                                title : 'ID',
-                                minWidth : 100,
-                                templet : function(data) {
-                                    // if (data.state == 0) {
-                                    // return "正常";
-                                    // }
-                                    // if (data.state == 1) {
-                                    // return "<font color='red'>刪除</font>";
-                                    // }
-                                    // return "数据异常";
-                                    return data.id;
-                                }
-                            }, {
-                                field : 'name',
-                                title : '部门名称',
-                                minWidth : 100,
-                                templet : function(data) {
-                                    // if (data.state == 0) {
-                                    // return "正常";
-                                    // }
-                                    // if (data.state == 1) {
-                                    // return "<font color='red'>刪除</font>";
-                                    // }
-                                    // return "数据异常";
-                                    return data.name;
-                                }
-                            }, {
-                                field : 'companyid',
-                                title : '公司ID',
-                                minWidth : 100,
-                                templet : function(data) {
-                                    // if (data.state == 0) {
-                                    // return "正常";
-                                    // }
-                                    // if (data.state == 1) {
-                                    // return "<font color='red'>刪除</font>";
-                                    // }
-                                    // return "数据异常";
-                                    return data.companyid;
-                                }
-                            }, {
-                                field : 'gmtCreate',
-                                title : '创建时间',
-                                minWidth : 100,
-                                templet : function(data) {
-                                    // if (data.state == 0) {
-                                    // return "正常";
-                                    // }
-                                    // if (data.state == 1) {
-                                    // return "<font color='red'>刪除</font>";
-                                    // }
-                                    // return "数据异常";
-                                    return data.gmtCreate;
-                                }
-                            }, {
-                                field : 'parentid',
-                                title : '父级ID',
-                                minWidth : 100,
-                                templet : function(data) {
-                                    // if (data.state == 0) {
-                                    // return "正常";
-                                    // }
-                                    // if (data.state == 1) {
-                                    // return "<font color='red'>刪除</font>";
-                                    // }
-                                    // return "数据异常";
-                                    return data.parentid;
-                                }
-                            }, {
-                                field : 'gmtModified',
-                                title : '修改时间',
-                                minWidth : 100,
-                                templet : function(data) {
-                                    // if (data.state == 0) {
-                                    // return "正常";
-                                    // }
-                                    // if (data.state == 1) {
-                                    // return "<font color='red'>刪除</font>";
-                                    // }
-                                    // return "数据异常";
-                                    return data.gmtModified;
-                                }
-                            }, {
-                                field : 'companyname',
-                                title : '公司名称',
-                                minWidth : 100,
-                                templet : function(data) {
-                                    // if (data.state == 0) {
-                                    // return "正常";
-                                    // }
-                                    // if (data.state == 1) {
-                                    // return "<font color='red'>刪除</font>";
-                                    // }
-                                    // return "数据异常";
-                                    return data.companyname;
-                                }
-                            } ] ],
-                    page : true, // 开启分页
-                    request : laypagerequest,
-                    response : laypageresponse,
-                    where : $("#searchForm").serializeJSON()
-                });// table
-            });
+            Menu.table.refresh();
+            // layui.use('table', function() {
+            // var table = layui.table;
+            // table.render({
+            // elem : '#oaDepartmenttable' // 选定是那个DIV
+            // ,
+            // url : rootPath + '/oa/department_do/list.do',
+            // cols : [
+            // [
+            // {
+            // type : 'checkbox'
+            // }, {
+            // field : 'id',
+            // title : 'ID',
+            // minWidth : 100,
+            // templet : function(data) {
+            // return data.id;
+            // }
+            // }, {
+            // field : 'name',
+            // title : '部门名称',
+            // minWidth : 100,
+            // templet : function(data) {
+            // return data.name;
+            // }
+            // }, {
+            // field : 'companyid',
+            // title : '公司ID',
+            // minWidth : 100,
+            // templet : function(data) {
+            // return data.companyid;
+            // }
+            // }, {
+            // field : 'gmtCreate',
+            // title : '创建时间',
+            // minWidth : 100,
+            // templet : function(data) {
+            // return data.gmtCreate;
+            // }
+            // }, {
+            // field : 'parentid',
+            // title : '父级ID',
+            // minWidth : 100,
+            // templet : function(data) {
+            // return data.parentid;
+            // }
+            // }, {
+            // field : 'gmtModified',
+            // title : '修改时间',
+            // minWidth : 100,
+            // templet : function(data) {
+            // return data.gmtModified;
+            // }
+            // }, {
+            // field : 'companyname',
+            // title : '公司名称',
+            // minWidth : 100,
+            // templet : function(data) {
+            // return data.companyname;
+            // }
+            // } ] ],
+            // page : true, // 开启分页
+            // request : laypagerequest,
+            // response : laypageresponse,
+            // where : $("#searchForm").serializeJSON()
+            // });// table
+            // });
         }
     }
 // method
 });
-vm.reload();
+var Menu = {
+    id : "oaDepartmenttable",
+    table : null,
+    layerIndex : "0"
+};
+/**
+ * 初始化表格的列
+ */
+Menu.initColumn = function() {
+    var columns = [
+        {
+            field : 'selectItem',
+            radio : true
+        }, {
+            title : '部门ID',
+            field : 'id',
+            visible : false,
+            align : 'center',
+            valign : 'middle',
+            width : '80px'
+        }, {
+            title : '部门名称',
+            field : 'name',
+            align : 'center',
+            valign : 'middle',
+            sortable : true,
+            width : '180px'
+        } ]
+    return columns;
+};
+function getMenuId() {
+    var selected = $('#oaDepartmenttable').bootstrapTreeTable('getSelections');
+    if (selected.length == 0) {
+        alert("请选择一条记录");
+        return false;
+    } else {
+        return selected[0].id;
+    }
+}
+$(function() {
+    var colunms = Menu.initColumn();
+    var table = new TreeTable(Menu.id, rootPath + '/oa/department_do/treelist.do', colunms);
+    table.setExpandColumn(2);
+    table.setIdField("id");
+    table.setCodeField("id");
+    table.setParentCodeField("parentid");
+    table.setExpandAll(true);
+    table.init();
+    Menu.table = table;
+});
+// vm.reload();
